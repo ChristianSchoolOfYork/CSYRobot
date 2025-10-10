bc5a8b205a8d18cd5bdf4555237c3ddb2af6bd4d
CSY Student <noreply@csyonline.com>
2025-10-10 16:58:30 -0400

    Add changes for TuneHeading

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/TuneHeading.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/TuneHeading.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.tuning;
 
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -7,20 +9,26 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.MecanumDrive;
 
+@SuppressWarnings("unused")
 @TeleOp
 public class TuneHeading extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
+        TelemetryPacket tp = new TelemetryPacket();
         MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0,0,0));
         waitForStart();
         while (opModeIsActive()) {
-            Actions.runBlocking(
-                    drive.actionBuilder(new Pose2d(0, 0, 0))
-                            .turn(Math.PI/2)
-                            .waitSeconds(3)
-                            .turn(-Math.PI/2)
-                            .waitSeconds(3)
-                            .build());
+            telemetry.addData("Heading", Math.toDegrees(drive.localizer.getPose().heading.toDouble()));
+            telemetry.update();
+
+            drive.updatePoseEstimate();
+            Actions.runBlocking( drive.actionBuilder(drive.localizer.getPose())
+                    .turn(Math.toRadians(-90))
+                    .waitSeconds(3)
+                    .turn(Math.toRadians(90))
+                    .waitSeconds(3)
+                    .build()
+            );
         }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/TuningActions.java
+++
@@ -1,7 +0,0 @@
-package org.firstinspires.ftc.teamcode.tuning;
-
-import com.acmerobotics.roadrunner.Action;
-@SuppressWarnings("unused")
-public class TuningActions {
-
-}
