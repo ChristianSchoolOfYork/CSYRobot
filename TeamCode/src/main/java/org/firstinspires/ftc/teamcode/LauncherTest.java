659e4151dbdaa92b719d3d7e9eb93018ce190e67
CSY Student <noreply@csyonline.com>
2025-10-18 09:27:01 -0400

    Add Launcher test, Update Robot USB facing Direction

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LauncherTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LauncherTest.java
@@ -9,29 +9,45 @@ public class LauncherTest  extends LinearOpMode {
 
     @Override
     public void runOpMode() {
-         DcMotor leftDrive;
-         DcMotor rightDrive;
+        DcMotor leftDrive;
+        DcMotor rightDrive;
 
-        leftDrive  = hardwareMap.get(DcMotor.class, "leftLauncher");
+        leftDrive = hardwareMap.get(DcMotor.class, "leftLauncher");
         rightDrive = hardwareMap.get(DcMotor.class, "rightLauncher");
 
+        leftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         waitForStart();
 
         double leftPower = 0.0;
-        boolean is_pressed  = false;
+        boolean is_up_pressed = false;
+        boolean is_down_pressed = false;
 
         while (opModeIsActive()) {
+            telemetry.addData("Power level", leftPower);
 
             leftDrive.setPower(leftPower);
             rightDrive.setPower(-leftPower);
 
-            if (gamepad1.dpad_up && !is_pressed) {
+            if (gamepad1.dpad_up && !is_up_pressed) {
                 leftPower = leftPower + 0.1;
-                is_pressed  = true;
+                is_up_pressed = true;
+            } else if (!gamepad1.dpad_up && is_up_pressed) {
+                is_up_pressed = false;
             }
-            else if (!gamepad1.dpad_up && is_pressed) {
-                is_pressed = false;
+
+            if (gamepad1.dpad_down && !is_down_pressed) {
+                leftPower = leftPower - 0.1;
+                is_down_pressed = true;
+            } else if (!gamepad1.dpad_down && is_down_pressed) {
+                is_down_pressed = false;
             }
+
+            if (gamepad1.b) {
+                leftPower = 0.0;
+            }
+
+            telemetry.update();
         }
     }
 }
