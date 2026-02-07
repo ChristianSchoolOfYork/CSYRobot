e697b43784343172d836e92d930fcd860806bc6b
CSY Student <noreply@csyonline.com>
2026-02-07 17:09:49 -0500

    Test all motors and servos, good news, guessed code works mostly

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelperClasses/Launcher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelperClasses/Launcher.java
@@ -3,6 +3,9 @@ package org.firstinspires.ftc.teamcode.HelperClasses;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.ServoController;
+
+import org.firstinspires.ftc.ftccommon.internal.manualcontrol.commands.ServoCommands;
 
 public class Launcher {
     final private Servo turnServo, angleServoLeft, angleServoRight, intakeServo;
@@ -10,20 +13,19 @@ public class Launcher {
     boolean intakeStage;
 
     public Launcher(HardwareMap hardwareMap){
-        turnServo = hardwareMap.get(Servo.class,"Turn Servo");
-        angleServoLeft = hardwareMap.get(Servo.class, "Angle Servo Left");
-        angleServoRight = hardwareMap.get(Servo.class, "Angle Servo Right");
-        intakeServo = hardwareMap.get(Servo.class, "Intake Servo");
-        intakeMotor = hardwareMap.get(DcMotor.class,"Intake Motor");
-        launcherMotor = hardwareMap.get(DcMotor.class, "Launcher Motor");
+        turnServo = hardwareMap.get(Servo.class,"turnServo");
+        angleServoLeft = hardwareMap.get(Servo.class, "angleServoLeft");
+        angleServoRight = hardwareMap.get(Servo.class, "angleServoRight");
+        intakeServo = hardwareMap.get(Servo.class, "intakeServo");
+        intakeMotor = hardwareMap.get(DcMotor.class,"intakeMotor");
+        launcherMotor = hardwareMap.get(DcMotor.class, "launcherMotor");
         intakeStage = false;
 
         intakeMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         launcherMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
 
-        //TODO: Reverse Motors If Needed
-        //TODO: Find Which Servo Needs To Be Reversed (assuming right for now)
         angleServoRight.setDirection(Servo.Direction.REVERSE);
+        angleServoLeft.setDirection(Servo.Direction.FORWARD);
     }
 
     //Launcher related classes
@@ -33,7 +35,7 @@ public class Launcher {
     public void launch(){
         launcherMotor.setPower(1);
     }
-    public void holdLaunch(){
+    public void stopLaunch(){
         launcherMotor.setPower(0);
     }
 
@@ -45,15 +47,30 @@ public class Launcher {
         intakeMotor.setPower(power);
     }
     public void nextLaunchStage(){
-        intakeServo.setPosition(intakeStage? 1 : 0);
+        final double min = 0,max = 1;
+        intakeServo.setPosition(intakeStage? max : min);
         intakeStage = !intakeStage;
     }
 
+    //debug classes
+    public void setIntakeServo(double pos){
+        intakeServo.setPosition(pos);
+    }
+
+    public double getIntakeServoPos(){
+        return intakeServo.getPosition();
+    }
+
     //Launch angle classes (private because they are to be accessed through the angle servos class anywhere else, as a pair
+    public double getAngleRight(){
+        return angleServoRight.getPosition();
+    }
+
     protected void angleRight(double position){
         angleServoRight.setPosition(position);
     }
-    protected double getAngleLeft(){
+
+    public double getAngleLeft(){
         return angleServoLeft.getPosition();
     }
     protected void angleLeft(double position){
@@ -65,10 +82,5 @@ public class Launcher {
         //Sets position to the current position + the new position, with bounds at 0 and 1
         turnServo.setPosition(Math.max(0, Math.min(1,turnServo.getPosition() + positonUpdate)));
     }
-
-
-
-
-
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Launchpad.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Launchpad.java
@@ -1,17 +1,26 @@
 package org.firstinspires.ftc.teamcode.TeleOp;
 
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
 import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.HelperClasses.AngleServos;
+import org.firstinspires.ftc.teamcode.HelperClasses.Launcher;
 
 public class Launchpad {
     Gamepad gamepad2;
-    public Launchpad(Gamepad gamepad2){
+    Launcher launcher;
+    AngleServos angleServos;
+
+    public Launchpad(Gamepad gamepad2, HardwareMap hardwareMap){
         this.gamepad2 = gamepad2;
+        this.launcher = new Launcher(hardwareMap);
+        angleServos = new AngleServos(launcher);
     }
 
-    public Action launchLoop(Action launchPlan, TelemetryPacket p){
-        boolean launchBusy = launchPlan != null && launchPlan.run(p);
-        return null;
+    public void launchLoop(Action launchPlan, TelemetryPacket p){
+
     }
 }
